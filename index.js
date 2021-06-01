35714ac2526b55d31da2f19da1c6a7d1d1ad3a30
Matt <Mattxiong_14@hotmail.com>
2021-06-01 11:42:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const toyota = {
+    id: 1,
+    name: "Supra",
+    maker: "HotWheel",
+    price: 1.50,
+    weight: 1.1,
+    length: 1.4
+}
+
+const ford = {
+    id: 2,
+    name: "Raptor",
+    maker: "Hot Wheel",
+    price: 1.50,
+    weight: 2.5,
+    length: 1.3
+}
+
+const dodge = {
+    id: 3,
+    name: "Viper",
+    maker: "Hot Wheel",
+    price: 1.50,
+    weight: 1.8,
+    length: 1.7
+}
