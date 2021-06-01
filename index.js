0495314624c261122a475e411f83091585e810a1
Matt <Mattxiong_14@hotmail.com>
2021-06-01 13:41:59 -0500

    edited the price. price was raised 5%

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cars = [
     id: 1,
     name: "Supra",
     maker: "Hot Wheel",
-    price: 1.50,
+    price: 1.52,
     weight: 1.1,
     length: 1.4
 },
@@ -19,7 +19,7 @@ const cars = [
     id: 3,
     name: "Viper",
     maker: "Hot Wheel",
-    price: 1.50,
+    price: 1.53,
     weight: 1.8,
     length: 1.7
 },
@@ -27,7 +27,7 @@ const cars = [
     id:4,
     name: "Corvette",
     maker: "Hot Wheel",
-    price: 1.50,
+    price: 1.58,
     weight: 1.9,
     length: 2.6
 },
@@ -35,13 +35,14 @@ const cars = [
     id:5,
     name: "NSX",
     maker: "Hot Wheel",
-    price: 1.50,
+    price: 1.55,
     weight: 2.6,
     length: 2.8
 }
 ]
 
 for (const _car of cars) {
-    console.log(`The ${_car.maker} ${_car.name} weighs ${_car.weight} pounds.`)
+    _car.price = _car.price + 0.05
+    console.log(`The ${_car.maker} ${_car.name} weighs ${_car.weight} pounds. It now cost ${_car.price} dollars.`)
 }
 
